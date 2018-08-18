4af807dab36c6c373015f6ab2cb28c39bc5a8d0e
VirtualSalesman <Mat.C.Yates@gmail.com>
2018-08-18 16:59:15 -0400

    gzip express

--- server/server.js
+++ server/server.js
@@ -6,6 +6,12 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
+app.get('*.js', function (req, res, next) {
+  req.url = req.url + '.gz';
+  res.set('Content-Encoding', 'gzip');
+  next();
+});
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'))
 });
