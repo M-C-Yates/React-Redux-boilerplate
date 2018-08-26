76e50fba8d19fbd649dd3d0bc90de208d59a30c3
VirtualSalesman <Mat.C.Yates@gmail.com>
2018-08-26 12:35:48 -0400

    more webpack

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,12 +3,19 @@ import * as firebase from 'firebase';
 
 const config = {
   apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+  authDomain: "budgetapp-3b626.firebaseapp.com",
+  databaseURL: "https://budgetapp-3b626.firebaseio.com",
+  projectId: "budgetapp-3b626",
+  storageBucket: "budgetapp-3b626.appspot.com",
+  messagingSenderId: "628908091853"
 };
+
+
+firebase.initializeApp(config);
+
+const database = firebase.database();
+
+export { firebase, database as default };
 
 // const config = {
 //   apiKey: "AIzaSyAapA5P8gWC83aHK26xgDDMzIBC4dw914w",
@@ -19,11 +26,9 @@ const config = {
 //   messagingSenderId: "124728096317"
 // };
 
-firebase.initializeApp(config);
 
-const database = firebase.database();
-
-export { firebase, database as default };
-
-
-
+  // authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  // databaseURL: process.env.FIREBASE_DATABASE_URL,
+  // projectId: process.env.FIREBASE_PROJECT_ID,
+  // storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  // messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
