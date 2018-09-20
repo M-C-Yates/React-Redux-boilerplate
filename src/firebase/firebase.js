afd3389dfd5c773d3c693f2a9235ce30c8500c05
Matthew Yates <33201611+M-C-Yates@users.noreply.github.com>
2018-09-20 12:04:25 -0400

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,14 +2,13 @@ import * as firebase from 'firebase';
 
 
 const config = {
-  apiKey: 'AIzaSyBpHhCypfsGfhgVQPc_boktY4tAr4Y1lxU',
+  apiKey: apiKey: `${process.env.FIREBASE_API_KEY}`,
   authDomain: "budgetapp-3b626.firebaseapp.com",
   databaseURL: "https://budgetapp-3b626.firebaseio.com",
   projectId: "budgetapp-3b626",
   storageBucket: "budgetapp-3b626.appspot.com",
   messagingSenderId: "628908091853"
 };
-// apiKey: `${process.env.FIREBASE_API_KEY}`,
 
 firebase.initializeApp(config);
 
